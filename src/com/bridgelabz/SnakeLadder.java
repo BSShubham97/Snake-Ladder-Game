4d33078f646af05ab937941f39ea05da16e0a7b3
Shubham Bandagale <sbandagale18@gmail.com>
2021-08-12 13:40:37 +0530

    UC-1

---
+++ src/com/bridgelabz/SnakeLadder.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+import java.lang.reflect.Array;
+
+public class SnakeLadder<board, START, END> {
+    public static void main(String[] args) {
+
+
+    int board[]=new int[100];
+    int START=0;
+    int END;
+        END = board.length;
+        int i;
+    System.out.println("Welcome to Snake and Laddder game");
+    System.out.println("----------------------------------");
+
+    System.out.println("START POSITION:"+START);
+    System.out.println("END POSITION:"+END);
+    }
+}
